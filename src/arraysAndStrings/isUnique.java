d615eb89a773b5a0ae7896c060b2a4227e43d056
Jay Bhanushali <bhanushalij@pmc.local>
2023-06-18 11:56:43 +0530

    added comments and updated the boolean array size from 256 to 128

--- src/arraysAndStrings/isUnique.java
+++ src/arraysAndStrings/isUnique.java
@@ -8,14 +8,14 @@ public class isUnique {
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		String S=sc.nextLine();
-		System.out.print(lowerCaseIsUnique(S));
+		System.out.print(isUnique(S));
 	}
 	
 	public static boolean isUnique(String s) {
-		if(s.length()>256) {
+		if(s.length()>128) {
 			return false;
 		}
-		boolean[] char_set=new boolean[256];
+		boolean[] char_set=new boolean[128];
 		for(int i=0;i<s.length();i++) {
 			int value=s.charAt(i);
 			if(char_set[value]) {
@@ -27,11 +27,12 @@ public class isUnique {
 	}
 	
 
-	
+	// if we know given string contains only lowercase letters a-z we can write the below code.
 	public static boolean lowerCaseIsUnique(String s) {
 		int checker=0;
 		for(int i=0;i<s.length();i++) {
 			int value=s.charAt(i)-'a';
+			char ch=s.charAt(i);
 			if((checker & (1<<value)) > 0) {
 				return false;
 			}
